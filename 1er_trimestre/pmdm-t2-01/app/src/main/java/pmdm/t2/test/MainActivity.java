c1aeb67fac90587732a56021485752a9f40117dc
jbocbar294 <jbocbar294@iesmartinezm.es>
2023-10-06 17:38:52 +0200

    First Commit

---
+++ 1er_trimestre/pmdm-t2-01/app/src/main/java/pmdm/t2/test/MainActivity.java
@@ -0,0 +1,39 @@
+package pmdm.t2.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onPostResume() {
+        super.onPostResume();
+    }
+
+    protected void onPause() {
+        super.onPause();
+    }
+
+    public void onStop() {
+        super.onStop();
+    }
+
+    public void onStart() {
+        super.onStart();
+    }
+
+
+}
